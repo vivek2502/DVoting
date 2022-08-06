d2ecbd230e8a85ec6cb6115ed68ece0ad171d41b
vivek2502 <vivekgupta.mailme@gmail.com>
2022-08-06 15:04:13 +0530

    Updates

---
+++ precache-manifest.dbefed829c6a3daa963d6198cf6c32a0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea016e9e6d4e00bde1a8d6a6b9a5356",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7499b9afb274b422d01",
+    "url": "./static/css/2.5e11034a.chunk.css"
+  },
+  {
+    "revision": "fbf16a47c538590b2cb3",
+    "url": "./static/css/main.4c72d6c3.chunk.css"
+  },
+  {
+    "revision": "f7499b9afb274b422d01",
+    "url": "./static/js/2.3c9299ad.chunk.js"
+  },
+  {
+    "revision": "910ed50f781388aa1e3c206f0b19aaba",
+    "url": "./static/js/2.3c9299ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf16a47c538590b2cb3",
+    "url": "./static/js/main.46826572.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "b1455663ebd72fea96575fae5f48d270",
+    "url": "./static/media/aap.b1455663.png"
+  },
+  {
+    "revision": "8d36bd5eed1a54aec38ab960e65b81dc",
+    "url": "./static/media/bjp.8d36bd5e.png"
+  },
+  {
+    "revision": "67c12fed1e18039bc1513d6b2f820448",
+    "url": "./static/media/kejriwal.67c12fed.jpg"
+  },
+  {
+    "revision": "8cdde503657e2a1f6e81ff8ec0d636d7",
+    "url": "./static/media/modi.8cdde503.jpg"
+  },
+  {
+    "revision": "d99f779eaa72817d4e74f55d2e159166",
+    "url": "./static/media/priyanka.d99f779e.jpg"
+  }
+]);
